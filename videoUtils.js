9de8b28b6628da5ee67955e8532a992855a041ec
vkboss49264 <118330284+vkboss49264@users.noreply.github.com>
2025-02-16 14:12:15 +0530

    Delete videoUtils.js

--- videoUtils.js
+++
@@ -1,7 +0,0 @@
-// videoUtils.js
-
-export function getYouTubeVideoId(url) {
-    const regex = /(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/)([^"&?\/\s]{11}))/;
-    const match = url.match(regex);
-    return match ? match[1] : null;
-}
