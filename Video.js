5661b9272e83fd3af22293c9bad06fcecbe679f9
vkboss49264 <118330284+vkboss49264@users.noreply.github.com>
2025-02-16 14:10:43 +0530

    Delete Video.js

--- Video.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const videoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  videoId: { type: String, required: true }, // Store only the YouTube Video ID
-});
-
-module.exports = mongoose.model("Video", videoSchema);
